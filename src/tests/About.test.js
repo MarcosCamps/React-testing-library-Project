d9d4fc1eba41ecce80b13942d6d026d8e5d41c95
Marcos Campos <marxmyers@icloud.com>
2022-03-22 17:37:23 -0300

    Adicionando requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+import About from '../components/About';
+
+describe('Teste as informações sobre a Pokédex', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const heading = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(heading).toBeInTheDocument();
+  });
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const findPOne = screen.getByText(/encyclopedia/i);
+    const findPTwo = screen.getByText(/details/i);
+    expect(findPOne).toBeInTheDocument();
+    expect(findPTwo).toBeInTheDocument();
+  });
+  it('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
