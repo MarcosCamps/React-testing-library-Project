7b4bfbd7084b176d73224894dc9f58620e630758
Marcos Campos <marxmyers@icloud.com>
2022-03-22 18:09:38 -0300

    Adicionando requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './RenderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Teste se a pessoa não tem pokemons favoritos', () => {
+  it('Exibir No favorite pokemon found, se a pessoa não tiver pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const text = screen.getByText('No favorite pokemon found');
+    expect(text).toBeInTheDocument();
+  });
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites/');
+    const haveFavorite = screen.getByRole('heading',
+      { name: /Favorite pokémons/i, level: 2 });
+    expect(haveFavorite).toBeInTheDocument();
+    userEvent.click(haveFavorite);
+  });
+});
